56bc9f47fe4c95219acb320f9136825f8cd9c62c
Saurabh Sain <visual.sain100486@gmail.com>
2023-11-16 11:42:21 +0530

    holow Diamond Pyramid

--- hollowDiamondPyramid.dart
+++ hollowDiamondPyramid.dart
@@ -17,9 +17,24 @@ hollowDiamondPyramid() {
   var row = 4;
   var space = " ";
 
-  for (var i = row; i >= 1; i--) {
-    stdout.write("${space * (row - i)}");
+  for (var i = 1; i <= row; i++) {
+    stdout.write("${space * (row - i + 1)}");
     stdout.write("*");
+    if (i > 1) {
+      stdout.write("${space * ((i - 1) * 2 - 1)}");
+      stdout.write("*");
+    }
+    print("");
   }
-  print("");
+
+  for (var j = row - 1; j >= 1; j--) {
+    stdout.write("${space * (row - j + 1)}");
+    stdout.write("*");
+
+    if (j > 1) {
+      stdout.write("${space * ((j - 1) * 2 - 1)}");
+      stdout.write("*");
+    }
+    print("");
+  }
 }
